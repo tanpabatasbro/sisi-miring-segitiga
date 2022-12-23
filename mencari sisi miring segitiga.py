1f4dd9ddec7d32e6395714f4f024994b4a7a021c
tanpabatasbro <115479649+tanpabatasbro@users.noreply.github.com>
2022-12-23 21:29:05 +0800

    Add files via upload

---
+++ mencari sisi miring segitiga.py
@@ -0,0 +1,9 @@
+# MENCARI SISI MIRING SEGITIGA
+
+import math
+
+a = int(input("Masukkan Tinggi segitiga : "))
+b = int(input("Masukkan alas segitiga : "))
+c = int(math.sqrt(a**2 + b**2))
+
+print(f"Sisi Miring Segitiga = {c}")
